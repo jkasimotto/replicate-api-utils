b4035934d7f21c5981856c0337fb3bdbf7495e39
julianotto <julian.kasimir.otto@gmail.com>
2023-02-17 09:07:51 +1100

    Add initial setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='replicate_api_utils',
+    version='0.1.0',
+    author='Your Name',
+    description='Python package for accessing the Replicate API',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+        'replicateai',
+        # any other necessary dependencies
+    ],
+)
