b413f677ff17b3364e9ac50b5059db1209afe390
julianotto <julian.kasimir.otto@gmail.com>
2023-02-17 11:56:42 +1100

    0.1.2. Patch tqdm install
    Added tqdm to the install_requires in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     install_requires=[
         'click',
         'replicate',
-        # any other necessary dependencies
+        'tqdm',
     ],
     entry_points={
         "console_scripts": [
